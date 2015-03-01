457cfd049389c17ae696e27f751e3e6d01a4e00c
Naoya Ito <i.naoya@gmail.com>
2015-03-01 16:15:31 +0900

    Tweak zoom factor

--- bin/screenshot.js
+++ bin/screenshot.js
@@ -2,7 +2,7 @@ var props = {
   // Chrome
   pc:{
     ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36",
-    viewport: { width: 1280, height: 720 }
+    viewport: { width: 2560, height: 1440 }
   },
   // iPhone 5, iOS 8.0.2
   mobile: {
@@ -17,6 +17,7 @@ var prop = casper.cli.args[1] == "mobile" ? props.mobile : props.pc;
 
 casper.start();
 casper.userAgent(prop.ua);
+casper.zoom(2);
 casper.open(casper.cli.args[0]).viewport(prop.viewport.width, prop.viewport.height).then(function () {
   console.log(this.captureBase64('png'));
 });
