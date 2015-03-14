43650fa4023c0ab095c412d898b32c4ede9791ff
Naoya Ito <i.naoya@gmail.com>
2015-03-14 11:42:13 +0900

    Use React.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,120 @@
 var App = React.createClass({displayName: "App",
   getInitialState: function() {
+    return {
+      screenshots: [],
+      loading:     false,
+      url:         null
+    };
+  },
+  takeScreenShot: function() {
+    if (!this.state.url) {
+      window.alert('URLを入力してね');
+      return;
+    }
+
+    this.setState({ loading: true });
+
+    var request = '/screenshot?url=' + encodeURIComponent(this.state.url);
+    // if ($('#is-mobile').prop('checked')) {
+    //   request += '&mobile=true';
+    // }    
+
+    // FIXME: get rid of jQuery
+    $.ajax({
+      url: request
+    }).done(function(data) {
+      var screenshot = { id:3, data:data };
+      this.setState({
+        screenshots: [screenshot].concat(this.state.screenshots),
+        loading:     false
+      });
+    }.bind(this)).fail(function(data) {
+      this.setState({ loading: false });
+    });
+  },
+  handleUrl: function(value) {
+    this.setState({ url: value });
   },
   render: function() {
     return (
-      React.createElement("h1", null, "Follow Me, Follow You")
+      React.createElement("div", null, 
+        React.createElement("h1", null, "スクショ撮る君"), 
+        React.createElement(Form, {onSubmit: this.takeScreenShot, onUrlChange: this.handleUrl}), 
+        React.createElement(Indicator, {loading: this.state.loading}), 
+        React.createElement(ScreenShotList, {screenshots: this.state.screenshots})
+      )
     );
   }
 });
+
+var Form = React.createClass({displayName: "Form",
+  propTypes: {
+    onUrlChange: React.PropTypes.func.isRequired,
+    onSubmit:    React.PropTypes.func.isRequired,
+  },
+  handleUrlChange: function(e) {
+    this.props.onUrlChange(e.target.value);
+  },
+  handleButton: function() {
+    // $('#indicator').show();
+    this.props.onSubmit();
+  },
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement("input", {id: "url", type: "text", placeholder: "http://", onChange: this.handleUrlChange}), 
+        React.createElement("button", {id: "btn-screenshot", onClick: this.handleButton}, "撮る")
+      )
+    );
+  }
+});
+
+var Indicator = React.createClass({displayName: "Indicator",
+  propTypes: {
+    loading: React.PropTypes.bool.isRequired
+  },
+  render: function() {
+    if (this.props.loading) {
+      return(      
+        React.createElement("span", null, React.createElement("img", {id: "indicator", src: "/images/ajax-loader.gif"}))
+      );
+    } else {
+      return (
+        React.createElement("span", null)
+      );
+    }
+  }
+});
+
+var ScreenShotList = React.createClass({displayName: "ScreenShotList",
+  render: function() {
+    var screenshots = this.props.screenshots.map(function (screenshot) {
+      return (
+        React.createElement(ScreenShot, {screenshot: screenshot, key: screenshot.id})
+      );
+    }.bind(this));
+    return (
+      React.createElement("div", {id: "result"}, screenshots)
+    );
+  }
+});
+
+var ScreenShot = React.createClass({displayName: "ScreenShot",
+  propTYpes: {
+    screenshot: React.PropTypes.shape({
+      id: React.PropTypes.number.isRequired,
+      data: React.PropTypes.string.isRequired
+    })
+  },
+  render: function() {
+    var src = "data:image/png;base64," + this.props.screenshot.data;
+    return (
+      React.createElement("img", {src: src})
+    );
+  }
+});
+
+React.render(
+  React.createElement(App, null),
+  document.getElementById('app-container')
+);
