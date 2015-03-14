0d27cf03d3026c6d9178a8b6ec887c766d59b990
Naoya Ito <i.naoya@gmail.com>
2015-03-14 12:09:25 +0900

    Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
-var gulp = require('gulp');
-var react = require('gulp-react');
-var plumber = require('gulp-plumber');
+var gulp       = require('gulp');
+var plumber    = require('gulp-plumber');
+var browserify = require('browserify');
+var reactify   = require('reactify');
+var source     = require('vinyl-source-stream');
 
-gulp.task('react', function () {
-  return gulp.src('./jsx/**/*.jsx')
-      .pipe(plumber())
-      .pipe(react())
-      .pipe(gulp.dest('./public/js/'));
+gulp.task('build', function() {
+  browserify({
+    entries: [ './jsx/application.jsx' ],
+    transform: [reactify]
+  })
+      .bundle()
+      .pipe(source('application.js'))
+      .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./jsx/**/*.jsx'], ['react']);
+  gulp.watch(['./jsx/**/*.jsx'], ['build']);
 });
--- jsx/application.jsx
+++ jsx/application.jsx
@@ -1,3 +1,5 @@
+var React = require('react');
+
 var App = React.createClass({
   getInitialState: function() {
     return {
